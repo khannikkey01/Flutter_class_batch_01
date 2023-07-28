54529540416e7fd998e71aaf0fd2ed8eb2abc638
khannikkey01 <khannikkey@gmail.com>
2023-07-28 08:28:02 -0700

    Added data types with some Extra Details

---
+++ 03-Data_types_01.dart
@@ -0,0 +1,26 @@
+void main() {
+  String nameOne = "Nadeem";
+  var nameTwo = "Qureshi";
+  print("This is nameOne variable: $nameOne");
+  print("This is how to escape \$ a dollar sign");
+  print("The length of nameTwo is ${nameTwo.length}");
+  print("The upperCase version of nameOne is ${nameOne.toUpperCase()}");
+  var s1 = "This is a string";
+  print(s1);
+  print(s1.toUpperCase());
+  var upperS1 = s1.toUpperCase();
+  print(s1);
+  print(upperS1);
+
+  var s2 = "This is a string";
+  print(s2);
+  print(s2.toUpperCase());
+  var upperS2 = s2.toUpperCase();
+  print(s2);
+  print(upperS2);
+
+  var theTruth = true;
+  var theFalse = false;
+  print(theTruth);
+  print(theFalse);
+}
